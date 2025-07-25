5d88c4669254ea85638706432a7f4cf1e0b4b750
enes cicek <enes.cicek02@outlook.de>
2025-07-25 04:14:54 +0200

    pc 25.07

--- test/test.py
+++ test/test.py
@@ -1,23 +1,23 @@
 import logging
-
+import time
 import src.libs.logs as logs
 
 logs.init_logging(log_level=logging.DEBUG)
 
-#from src.scripts import bot_skinlist_checker, item_lister
+from src.scripts import item_lister
 import src.scripts.analytics as analytics
-#import src.scripts.price_adapter as price_adapter
-#import src.scripts.commission_updater as commission_updater
+import src.scripts.price_adapter as price_adapter
+import src.scripts.commission_updater as commission_updater
 #import src.scripts.link_purchases_to_offers as link_purchases_to_offers
 
 import pandas as pd
 
 def main():
-    #link_purchases_to_offers.main()
-    #commission_updater.main(use_existing_linked_purchases=True)
-    #price_adapter.main(use_existing_linked_purchases=True, use_current_bot_skinlist=True)
     analytics.main(use_existing_linked_purchases=False)
-    #bot_skinlist_checker.main()
-    #item_lister.main()
+    commission_updater.main(use_existing_linked_purchases=True)
+    time.sleep(120)
+    price_adapter.main(use_existing_linked_purchases=False, use_current_bot_skinlist=True)
+    time.sleep(30)
+    analytics.main(use_existing_linked_purchases=False)
 
 main()
